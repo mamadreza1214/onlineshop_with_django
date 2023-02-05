056f9093b706b89249742c76672d2ef8e476087a
mamadreza1214 <m.ramezani6017@gmail.com>
2023-02-05 02:59:43 -0800

    create product app model

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
